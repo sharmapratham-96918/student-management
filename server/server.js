12ccfdc82aeb51e949e05d0a8044cec4a6827b32
sharmapratham-96918 <prathams96918@gmail.com>
2025-12-13 17:31:30 +0530

    production changes applied

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/dbConfig");
+const path = require('path')
+
 
 // Load env
 dotenv.config();
@@ -11,11 +13,21 @@ connectDB();
 
 const app = express();
 
-app.get("/" , (req,res) => {
-    res.json({
-        message :"WELLCOM TO STUDENT APIS"
-    })
-})
+
+// Default Route
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "/student-app/dist")));
+
+    app.get("/", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "student-app", "dist", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running... (development mode)");
+    });
+}
+
 
 // Middleware
 app.use(cors());
